059603d7fdf01ab2890d55caea20b34d41ef80ed
evanhosni <evankhaledhosni@gmail.com>
2022-03-27 22:52:55 -0700

    got servers working - one web dyno, one worker dyno

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ const options = {cors: {origin: "*"}}//TODO: change to only deployed link when w
 const io = require("socket.io")(server, options);
 var bowls
 
-io.on("connection", (socket) => {
+io.on("connection", () => {
     console.log("we're one, brother")
-    // setInterval(() => {
-    //     io.emit('bowlcount', bowls)
-    // }, 100)
-    io.on("doit", bowls => {
-        console.log(bowls)
-    })
 });
 server.listen(process.env.PORT || 3000);
 
@@ -25,7 +19,6 @@ function refresh() {
     Bowl.count({logging: false}).then(bowl => {//TODO: logging false doing anything?
         bowls = bowl
         console.log(bowls)
-        io.emit('doit', bowls)
     })
 }
 
