a563be23999147c9fc9466a36885031e845c458a
evanhosni <evankhaledhosni@gmail.com>
2022-03-27 21:38:51 -0700

    got servers working - one web dyno, one worker dyno

--- client.js
+++ client.js
@@ -1,11 +1,6 @@
 const socket = io("https://bowlbot-server.herokuapp.com/")
 var bowls = document.querySelector("#bowls")
 
-socket.on("init", (message) => {
-    console.log("init" + message)
-    bowls.innerHTML = message
-})
-
 socket.on("bowlcount", (message) => {
     console.log("init" + message)
     bowls.innerHTML = message
