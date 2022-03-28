47d8b90322ef22e1a5c176623ffc57ffc8e223d5
evanhosni <evankhaledhosni@gmail.com>
2022-03-27 20:38:44 -0700

    socketio stuff

--- bowlbot.js
+++ bowlbot.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 require("dotenv").config();
 const server = require("http").createServer()
 // const { sensitiveHeaders } = require("http2");//TODO: is this used?
-const options = {cors: {origin: "*"}}
-const io = require("socket.io")(server, options);
-io.on("connection", (socket) => {
-    console.log("we're one, brother")
-    Bowl.count().then(bowl => {
-        io.emit('bowlcount', bowl)
-    })
-});
-server.listen(process.env.PORT || 3000);
+// const options = {cors: {origin: "*"}}
+// const io = require("socket.io")(server, options);
+// io.on("connection", (socket) => {
+//     console.log("we're one, brother")
+//     Bowl.count().then(bowl => {
+//         io.emit('bowlcount', bowl)
+//     })
+// });
+// server.listen(process.env.PORT || 3000);
 
 const sequelize = require('./db/connection')
 const Op = sequelize.Op 
