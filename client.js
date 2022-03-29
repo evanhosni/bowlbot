75019f653943b36a349320b9fa0051135f96d69f
evanhosni <evankhaledhosni@gmail.com>
2022-03-29 14:06:46 -0700

    working on alternative to socketio

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io("https://bowlbot-server.herokuapp.com/")
+// const socket = io("https://bowlbot-server.herokuapp.com/")
 var bowls = document.querySelector("#bowls")
 
 socket.on("bowlcount", (message) => {
