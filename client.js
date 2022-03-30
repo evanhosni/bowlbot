5903992b55f1db7049be604d014ffad6608e2139
evanhosni <evankhaledhosni@gmail.com>
2022-03-29 17:53:42 -0700

    trying web dyno

--- client.js
+++ client.js
@@ -1,7 +1,15 @@
-// const socket = io("https://bowlbot-server.herokuapp.com/")
+const socket = io("https://bowlbot-server.herokuapp.com/")
 var bowls = document.querySelector("#bowls")
 
-// socket.on("bowlcount", (message) => {
-//     console.log(message)
-//     bowls.innerHTML = message
-// })
+setInterval(()=> {
+console.log(number)
+},1000)
+
+socket.on("bowlcount", (message) => {
+    console.log(message)
+    bowls.innerHTML = message
+})
+
+setInterval(()=> {
+    window.location.reload();
+},1000)
