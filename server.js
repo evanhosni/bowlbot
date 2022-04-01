7414c02e752caa4dea1550e4bfa01b05cd333b6f
evanhosni <evankhaledhosni@gmail.com>
2022-03-31 19:40:54 -0700

    trying intervals to keep site up

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('heroku-self-ping').default("https://evanhosni.github.io/bowlbot");//TODO yeet
+// require('heroku-self-ping').default("https://evanhosni.github.io/bowlbot");//TODO yeet
 const Discord = require("discord.js");
 require("dotenv").config();
 const server = require("http").createServer()
@@ -10,25 +10,13 @@ io.on("connection", (socket) => {
     Bowl.count().then(bowl => {
         io.emit('bowlcount', bowl)
     })
-    setInterval(()=> {//TODO yeet if dont work
-        Bowl.count().then(bowl => {
-            io.emit('bowlcount', bowl)
-        })
-    },10 * 1000 * 60)
 });
 server.listen(process.env.PORT || 3000);
-require('heroku-self-ping').default("https://evanhosni.github.io/bowlbot");//TODO yeet
+// require('heroku-self-ping').default("https://evanhosni.github.io/bowlbot");//TODO yeet
 // require('heroku-self-ping').default("https://bowlbot-server.herokuapp.com/");
 // console.log("server listenin on http://localhost:3000")//TODO yeet this
 // var ping = require('periodic-ping').ping;
 // ping({appName: "https://bowlbot-server.herokuapp.com/"});
-
-
-setInterval(()=> {//TODO yeet if dont work
-    Bowl.count().then(bowl => {
-        io.emit('bowlcount', bowl)
-    })
-},10 * 1000 * 60)
 
 const sequelize = require('./db/connection')
 const Op = sequelize.Op 
