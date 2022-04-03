92d3ab1cd5c006ebcc1d9dc5be69bc003e2b710f
evanhosni <evankhaledhosni@gmail.com>
2022-04-02 20:38:52 -0700

    updated website

--- client.js
+++ client.js
@@ -1,8 +1,15 @@
 const socket = io("https://bowlbot-server.herokuapp.com")
 // const socket = io("http://localhost:3000/")
-var bowls = document.querySelector("#bowls")
+var bowls = document.querySelector(".counter")
 
-socket.on("bowlcount", (message) => {
-    console.log(message)
-    bowls.innerHTML = message
-})
+// socket.on("bowlcount", (message) => {
+//     console.log(message)
+//     bowls.innerHTML = message
+// })
+number = 995
+bowls.innerHTML = number
+setInterval(() => {
+    number++
+    bowls.innerHTML = number
+},1000)
+
