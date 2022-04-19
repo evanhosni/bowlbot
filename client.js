a62dfe14183dfc7de14ac3f27c2dc85e14d1c982
evanhosni <evankhaledhosni@gmail.com>
2022-04-18 23:12:13 -0700

    help command, entry message, ability to turn rank on/off and leaderboard status dependent on rank

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-const socket = io("http://localhost:3000/")
-// const socket = io("https://bowlbot-server.herokuapp.com")
+// const socket = io("http://localhost:3000/")
+const socket = io("https://bowlbot-server.herokuapp.com")
 var connectedToServer
 var bowls = document.querySelector(".counter")
 var modal = document.querySelector('#modal')
