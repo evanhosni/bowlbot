bc21e701cb5de405b94ef1b4cd7a40d35edf5e1e
evanhosni <evankhaledhosni@gmail.com>
2022-06-15 20:27:27 -0700

    added missing express static

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 // app.use(express.urlencoded({extended:true})) //TODO: what does this do?
 // app.use(express.json()) //TODO: what does this do?
+app.use(express.static(__dirname))
 app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '/index.html'))
 );
