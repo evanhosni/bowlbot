c79a11cb90ac290e7af90f7a2a570d5e847b4b31
evanhosni <evankhaledhosni@gmail.com>
2023-05-06 16:56:22 -0700

    brings back leaderboards db

--- db/connection.js
+++ db/connection.js
@@ -2,29 +2,19 @@ const Sequelize = require('sequelize')
 require('dotenv').config()
 let sequelize
 
-
-sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-        dialect: 'mysql',
-        host: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-        dialectOptions: {
-            socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
-        },
-    }
-)
-
-// // For connecting to local db
-// sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//         dialect: 'mysql',
-//         host: 'localhost',
-//     }
-// )
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
+        {
+            dialect: 'mysql',
+            host: 'localhost',
+            // port: 3306
+        }
+    )
+}
 
 module.exports = sequelize
--- db/models.js
+++ db/models.js
@@ -1,39 +1,24 @@
 const Sequelize = require("sequelize")
 const sequelize = require('./connection')
 
-// const Server = sequelize.define("server", {
-//     id: {
-//         type: Sequelize.BIGINT,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     name: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     rank: {
-//         type: Sequelize.BOOLEAN,
-//         defaultValue: false
-//     }
-// })
-
-// const Bowl = sequelize.define("bowl", {})
-
-// Server.hasMany(Bowl)
-
-// module.exports = {Server, Bowl}
-
-const Bowls = sequelize.define("bowls", {
-    // id: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     primaryKey: true,
-    //     defaultValue: "total"
-    // },
-    // total: {
-    //     type: Sequelize.BIGINT,
-    //     allowNull: false
-    // }
+const Server = sequelize.define("server", {
+    id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rank: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    }
 })
 
-module.exports = {Bowls}
+const Bowl = sequelize.define("bowl", {})
+
+Server.hasMany(Bowl)
+
+module.exports = {Server, Bowl}
