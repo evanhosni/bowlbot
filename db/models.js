44e1abe27e0b7a7ed416902344934486dbc841c0
evanhosni <evankhaledhosni@gmail.com>
2023-05-07 20:58:35 -0700

    updates seed.js

--- db/models.js
+++ db/models.js
@@ -26,7 +26,7 @@ const Bowl = sequelize.define("bowl", {
         autoIncrement: true
     },
     schmokedAt: {
-        type: 'TIMESTAMP',
+        type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         allowNull: false
     }
