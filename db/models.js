7bb4832f11140a112a3a69aba2425924aa0b0a54
evanhosni <evankhaledhosni@gmail.com>
2023-05-08 15:22:34 -0700

    updates model

--- db/models.js
+++ db/models.js
@@ -3,7 +3,7 @@ const sequelize = require('./connection')
 
 const Server = sequelize.define("server", {
     id: {
-        type: Sequelize.NUMERIC(2,0),
+        type: Sequelize.NUMERIC(9),
         primaryKey: true,
         allowNull: false
     },
